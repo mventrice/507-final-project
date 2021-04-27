3bbe8651d0272917d89faf6c8a673e3def75ddd9
Mariele Ventrice <marieleventrice@gmail.com>
2021-04-27 11:02:46 -0400

    Update database

--- final_project_main.py
+++ final_project_main.py
@@ -67,7 +67,7 @@ class Track:
     # music_video: string
 
     '''
-    def __init__(self, genre="Unknown", track_name="Unknown track", spotify_track_id="None", track_popularity=0, artist_name="Unknown Artist", album_name="Unknown Album", spotify_url="Unknown", preview="Not available", spotify_artist_id="Unknown"):
+    def __init__(self, genre="Unknown", track_name="Unknown track", spotify_track_id="None", track_popularity=0, artist_name="Unknown Artist", album_name="Unknown Album", spotify_url="Unknown", preview="Not available", spotify_artist_id="Unknown", popularity=0):
         self.genre = genre
         self.track_name = track_name
         self.spotify_track_id = spotify_track_id
@@ -77,10 +77,10 @@ class Track:
         self.spotify_url = spotify_url
         self.preview = preview
         self.spotify_artist_id = spotify_artist_id
-        # self.danceability = int(danceability) * 100
+        self.popularity = popularity 
 
     def info(self):
-        return f"{self.track_name} ({self.album_name}) by {self.artist_name} – {self.spotify_artist_id}" 
+        return f"{self.track_name} ({self.album_name}) by {self.artist_name} – {self.popularity}" 
 
 class Artist:
     ''' A spotify artist
@@ -95,18 +95,30 @@ class Artist:
 
     artist_name: string
 
-    TADB_artist_id: string
+    image_url: string
+
 
     '''
-    def __init__(self, genre="Unknown", artist_name="Unknown Artist", spotify_artist_id="Unknown", TADB_artist_id="Unknown"):
+    def __init__(self, genre="Unknown", artist_name="Unknown Artist", spotify_artist_id="Unknown", image_url="Unknown"):
         self.genre = genre
         self.artist_name = artist_name
-        self.TADB_artist_id = TADB_artist_id
         self.spotify_artist_id = spotify_artist_id
-        # self.danceability = int(danceability) * 100
+        self.image_url = image_url
 
     def info(self):
         return f"{self.artist_name} ({self.spotify_artist_id})"
+
+class TrackFeatureProfile:
+
+    def __init__(self, spotify_track_id="Unknown", acousticness=0.0, danceability=0.0, tempo=0.0): 
+        self.spotify_track_id = spotify_track_id
+        self.acousticness = acousticness
+        self.danceability = danceability
+        self.tempo = tempo
+
+    def info(self):
+        return f"SpotifyID{self.spotify_track_id}, Acousticness: {self.acousticness}, Dancesability: {self.danceability}, tempo: {self.tempo}"
+
 
 def open_cache():
     '''Opens the cache file if it exists and loads the JSON into
@@ -234,9 +246,15 @@ def get_folk_tracks():
     results = make_spotify_request_with_cache(baseurl, params)
     return results
 
+def get_hiphop_tracks():
+    baseurl="https://api.spotify.com/v1/search"
+    params={"q":"genre:hip-hop year:2000-2010", "type":"track", "limit":50}
+    results = make_spotify_request_with_cache(baseurl, params)
+    return results
+
 def get_emo_tracks():
     baseurl="https://api.spotify.com/v1/search"
-    params={"q":"genre:emo", "type":"track", "limit":50}
+    params={"q":"genre:emo year:2000-2010", "type":"track", "limit":50}
     results = make_spotify_request_with_cache(baseurl, params)
     return results
 
@@ -248,7 +266,25 @@ def get_indie_tracks():
 
 def get_punk_tracks():
     baseurl="https://api.spotify.com/v1/search"
-    params={"q":"genre:punk", "type":"track", "limit":50}
+    params={"q":"genre:punk year:2000-2010", "type":"track", "limit":50}
+    results = make_spotify_request_with_cache(baseurl, params)
+    return results
+
+def get_pop_tracks():
+    baseurl="https://api.spotify.com/v1/search"
+    params={"q":"genre:pop", "type":"track", "limit":50}
+    results = make_spotify_request_with_cache(baseurl, params)
+    return results
+
+def get_indie_pop_tracks():
+    baseurl="https://api.spotify.com/v1/search"
+    params={"q":"genre:indie pop", "type":"track", "limit":50}
+    results = make_spotify_request_with_cache(baseurl, params)
+    return results
+
+def get_alternative_tracks():
+    baseurl="https://api.spotify.com/v1/search"
+    params={"q":"genre:'alternative' year:1990-2000", "type":"track", "limit":50}
     results = make_spotify_request_with_cache(baseurl, params)
     return results
 
@@ -257,6 +293,13 @@ def get_spotify_artists(spotify_artist_id):
     baseurl="https://api.spotify.com/v1/artists"
     search_url = baseurl + "/" + spotify_artist_id
     results = make_spotify_artists_request_with_cache(search_url)
+    return results
+
+def get_track_audio_features(track_id):  
+    # track_id = track_object.spotify_track_id
+    baseurl="https://api.spotify.com/v1/audio-features"
+    search_url = baseurl + "/" + track_id
+    results = make_spotify_audio_features_request_with_cache(search_url)
     return results
 
 def create_track_objects(spotify_track_results):
@@ -271,7 +314,7 @@ def create_track_objects(spotify_track_results):
         artist_name = results_list[i]['artists'][0]['name']
         artist_id = results_list[i]['artists'][0]['id']
         track_id = results_list[i]['id']
-        track_object = Track(track_name=track_name, spotify_track_id=track_id, track_popularity=popularity, artist_name=artist_name, album_name=album_name, spotify_url=spotify_url, preview=preview_url, spotify_artist_id=artist_id)
+        track_object = Track(track_name=track_name, spotify_track_id=track_id, popularity=popularity, artist_name=artist_name, album_name=album_name, spotify_url=spotify_url, preview=preview_url, spotify_artist_id=artist_id)
         list_track_objects.append(track_object)
     return list_track_objects
 
@@ -281,45 +324,71 @@ def create_artist_objects(spotify_artist_results):
         artist_id = spotify_artist_results[i]['id']
         artist_name = spotify_artist_results[i]['name']
         genre = spotify_artist_results[i]['genres']
-        artist_object = Artist(artist_name=artist_name, spotify_artist_id=artist_id, genre=genre, TADB_artist_id="Unknown")
+        image_url = spotify_artist_results[i]['images'][0]['url']
+        artist_object = Artist(artist_name=artist_name, spotify_artist_id=artist_id, genre=genre, image_url=image_url)
         list_artist_objects.append(artist_object)
     return list_artist_objects
 
-def turn_artist_objects_to_lists(list_artist_objects):
-    new_artists_list = []
-    for artist in list_artist_objects:
-        single_artist_list = []
-        single_artist_list.append(artist.artist_name)
-        single_artist_list.append("?")
-        single_artist_list.append(artist.spotify_artist_id)
-        single_artist_list.append("?")
-        new_artists_list.append(single_artist_list)
-    return new_artists_list
+def create_track_features_objects(track_features_results):
+    list_features_objects = []
+    for i in range(len(track_features_results)):
+        track_id =  track_features_results[i]['id']
+        acousticness = float(track_features_results[i]['acousticness'])
+        danceability = float(track_features_results[i]['danceability'])
+        tempo = float(track_features_results[i]['tempo'])
+        features_object = TrackFeatureProfile(spotify_track_id=track_id, acousticness=acousticness, danceability=danceability, tempo=tempo)
+        list_features_objects.append(features_object)
+    return list_features_objects
 
+def remove_duplicate_tracks(list):
+   i = 0
+   while i < len(list):
+      j = i + 1
+      while j < len(list):
+         if list[i].spotify_track_id == list[j].spotify_track_id:
+            del list[j]
+         else:
+            j += 1
+      i += 1
 
-def turn_track_objects_to_lists(list_track_objects):
-    new_track_list = []
-    for track in list_track_objects:
-        single_track_list = []
-        single_track_list.append(track.track_name)
-        single_track_list.append(track.artist_name)
-        single_track_list.append(track.album_name)
-        single_track_list.append(track.preview)
-        single_track_list.append(track.spotify_url)
-        single_track_list.append("?")
-        single_track_list.append(track.spotify_artist_id)
-        single_track_list.append(track.spotify_track_id)
-        new_track_list.append(single_track_list)
-    return new_track_list
+def remove_duplicate_artists(list):
+   i = 0
+   while i < len(list):
+      j = i + 1
+      while j < len(list):
+         if list[i].spotify_artist_id == list[j].spotify_artist_id:
+            del list[j]
+         else:
+            j += 1
+      i += 1
 
-def get_track_audio_features(track_object):  
-    track_id = track_object.spotify_track_id
-    baseurl="https://api.spotify.com/v1/audio-features"
-    search_url = baseurl + "/" + track_id
-    results = make_spotify_audio_features_request_with_cache(search_url)
-    return results
+# def map_genres(artist_object):
+#     if 'rock' in artist_object.genre or 'emo' in artist_object.genre or 'modern rock' in artist_object.genre or 'pop punk' in artist_object.genre:
+#         artist_object.genre = 'rock'
+#     elif 'alt z' in artist_object.genre or "alternative" in artist_object.genre:
+#         artist_object.genre = "alternative"
+#     elif 'rap' in artist_object.genre or 'hip-hop' in artist_object.genre:
+#         artist_object.genre = "hip-hop"
+#     elif 'pop' in artist_object.genre:
+#         artist_object.genre = 'pop'
+#     elif 'indie' in artist_object.genre or 'folk' in artist_object.genre or 'indie_folk' in artist_object.genre:
+#         artist_object.genre = 'indie'
 
-# def get_spotify_artists_by_id()
+def map_genres(artist_object):
+    genre_string = str(artist_object.genre)
+    if 'emo' in genre_string or "pop punk" in genre_string:
+        artist_object.genre = "emo"
+    elif 'alternative' in genre_string:
+        artist_object.genre = "alternative"
+    elif 'indie folk' in genre_string or "indie" in genre_string:
+        artist_object.genre = "indie"
+    elif 'rock' in genre_string:
+        artist_object.genre = 'rock'
+    elif 'hip hop' in genre_string:
+        artist_object.genre = "hip-hop"
+    elif 'pop' in genre_string:
+        artist_object.genre = "pop"
+
 
 ############## CREATING TABLES & INSERTING DATA ##############
 
@@ -329,15 +398,16 @@ drop_tracks = '''
 
 create_tracks = '''
     CREATE TABLE IF NOT EXISTS "tracks" (
-        "Id"                INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        "TrackId"           INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
         "TrackName"         TEXT NOT NULL,
-        "AristName"         TEXT NOT NULL,
+        "ArtistName"        TEXT NOT NULL,
         "AlbumName"         TEXT,
         "SpotifyPreview"    TEXT,
         "SpotifyURL"        TEXT,
-        "Genre"             TEXT,
         "SpotifyArtistId"   TEXT,
-        "SpotifyTrackId"    TEXT NOT NULL
+        "SpotifyTrackId"    TEXT NOT NULL,
+        "Popularity"        INTEGER,
+        FOREIGN KEY(SpotifyArtistId) REFERENCES artists(SpotifyArtistId)
     );
 '''
 
@@ -347,13 +417,30 @@ drop_artists = '''
 
 create_artists = '''
     CREATE TABLE IF NOT EXISTS "artists" (
-        "Id"                INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-        "AristName"         TEXT NOT NULL,
+        "SpotifyArtistId"   TEXT PRIMARY KEY UNIQUE,
+        "ArtistName"        TEXT NOT NULL,
         "Genre"             TEXT,
-        "SpotifyArtistId"   TEXT NOT NULL,
-        "TadbArtistId"      TEXT NOT NULL
+        "ImageUrl"          TEXT
     );
 '''
+
+drop_features = '''
+    DROP TABLE IF EXISTS "features";
+'''
+
+create_features = '''
+    CREATE TABLE IF NOT EXISTS "features" (
+
+        "SpotifyTrackId"    TEXT PRIMARY KEY NOT NULL,
+        "Acousticness"      REAL,
+        "Danceability"      REAL,    
+        "Tempo"             REAL
+        );
+'''
+
+# cur.execute(drop_features)
+# cur.execute(create_features)
+# conn.commit()
 
 # cur.execute(drop_tracks)
 # cur.execute(create_tracks)
@@ -364,18 +451,6 @@ cur.execute(create_artists)
 conn.commit()
 
 
-
-insert_tracks = '''
-    INSERT INTO tracks
-    VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)
-'''
-
-insert_artists = '''
-    INSERT INTO artists
-    VALUES (NULL, ?, ?, ?, ?)
-'''
-
-
 folk_results = get_folk_tracks()
 list_folk_track_objects = create_track_objects(folk_results)
 
@@ -384,47 +459,96 @@ list_emo_track_objects = create_track_objects(emo_results)
 
 indie_results = get_indie_tracks()
 list_indie_track_objects = create_track_objects(indie_results)
-# # print(list_indie_track_objects[0].info())
 
 punk_results = get_punk_tracks()
 list_punk_track_objects = create_track_objects(punk_results)
-# # print(list_punk_track_objects[0].info())
 
-full_track_list = list_folk_track_objects + list_indie_track_objects + list_punk_track_objects + list_emo_track_objects
+hiphop_results = get_hiphop_tracks()
+list_hiphop_track_objects = create_track_objects(hiphop_results)
+
+pop_results = get_pop_tracks()
+list_pop_track_objects = create_track_objects(pop_results)
+
+indie_pop_results = get_indie_pop_tracks()
+list_indie_pop_track_objects = create_track_objects(indie_pop_results)
+
+alternative_results = get_alternative_tracks()
+list_alternative_track_objects = create_track_objects(alternative_results)
+
+full_track_list = list_folk_track_objects + list_indie_track_objects + list_punk_track_objects + list_emo_track_objects + list_hiphop_track_objects + list_indie_pop_track_objects + list_pop_track_objects + list_alternative_track_objects
+remove_duplicate_tracks(full_track_list)
+
 # print(full_track_list)
-##Combine the lists here##
+#Combine the lists here##
+# print(len(full_track_list))
+
+###POPULATE TRACKS TABLE###
+
+insert_tracks = '''
+    INSERT INTO tracks
+    VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)
+'''
+
+# for track in full_track_list:
+#     cur.execute(insert_tracks, [track.track_name, track.artist_name, track.album_name, track.preview, track.spotify_url, track.spotify_artist_id, track.spotify_track_id, track.popularity])
+# conn.commit()
 
 
-
-### Get Artists ######
+## Get Artists ######
 artist_list = []
 for track in full_track_list:
     spotify_artist_id = track.spotify_artist_id
     results = get_spotify_artists(spotify_artist_id)
     artist_list.append(results)
 
-##### Turn Artists Results into Objects ###
+#### Turn Artists Results into Objects ###
 
 list_artist_objects = create_artist_objects(artist_list)
-list_artists_lists = turn_artist_objects_to_lists(list_artist_objects)
+remove_duplicate_artists(list_artist_objects)
+for artist in list_artist_objects:
+    map_genres(artist)
 
-for artist in list_artists_lists:
-    cur.execute(insert_artists, artist)
+####INSERT INTO ARTISTS TABLE######
+
+insert_artists = '''
+    INSERT INTO artists
+    VALUES (?, ?, ?, ?)
+'''
+
+for artist in list_artist_objects:
+    cur.execute(insert_artists, [artist.spotify_artist_id, artist.artist_name, artist.genre, artist.image_url])
 conn.commit()
 
-list_of_track_lists = turn_track_objects_to_lists(full_track_list)
-# print(list_of_track_lists)
-
-for track in list_of_track_lists:
-    cur.execute(insert_tracks, track)
-conn.commit()
 
 
+#### GET FEATURES #####
 
-# for key, val in results.items():
-#     print (key)
-# audio_features = get_track_audio_features(list_track_objects[0])
-# print(audio_features)
+# features_list = []
+# for track in full_track_list:
+#     spotify_track_id= track.spotify_track_id
+#     results = get_track_audio_features(spotify_track_id)
+#     features_list.append(results)
+
+#### FEATURES TO OBJECTS#####
+
+# audio_features_objects = create_track_features_objects(features_list)
+# print(audio_features_objects[0].info())
+
+######INSERT INTO FEATURES TABLE #####
+insert_features = '''
+    INSERT INTO features
+    VALUES (?, ?, ?, ?)
+'''
+# for feature in audio_features_objects:
+#     cur.execute(insert_features, [feature.spotify_track_id, feature.acousticness, feature.danceability, feature.tempo])
+# conn.commit()
+
+
+
+
+
+
+
 
 
 # audio_features_baseurl = "https://api.spotify.com/v1/audio-features"
@@ -457,3 +581,8 @@ conn.commit()
 
 # genre_results = requests.get("https://api.spotify.com/v1/recommendations/available-genre-seeds", headers=HEADERS)
 # print(genre_results.text)
+
+# result = get_track_audio_features("3JvrhDOgAt6p7K8mDyZwRd")
+# print(result)
+
+
